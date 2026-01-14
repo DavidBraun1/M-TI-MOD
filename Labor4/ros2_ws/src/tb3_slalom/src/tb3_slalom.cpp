d1475a20b5f355fe67713815b6c3e0afc1b1320c
DavidBraun1 <dbraun023@gmail.com>
2026-01-14 22:04:24 +0100

    finished first version of Lab4

--- Labor4/ros2_ws/src/tb3_slalom/src/tb3_slalom.cpp
+++ Labor4/ros2_ws/src/tb3_slalom/src/tb3_slalom.cpp
@@ -37,18 +37,19 @@ private:
 
         double siny_cosp = 2.0 * (qw * qz + qx * qy);
         double cosy_cosp = 1.0 - 2.0 * (qy * qy + qz * qz);
-        current_yaw_ = std::atan2(siny_cosp, cosy_cosp);
+        current_yaw = std::atan2(siny_cosp, cosy_cosp);
     }
 
     void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
     {
         int total = msg->ranges.size();
                 
-        front_dist = get_min_distance(msg, 0, 15, total);      // ±15° vorne
+        front_dist = get_min_distance(msg, 0, 5, total);      // ±5° vorne
         min_dist = get_min_distance(msg, 0, 180, total);         // 360° herum
+        front_min_dist = get_min_distance(msg, 0, 90, total);     // 180° vorne
         
-        RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 1000,
-                             "Vorne: %.2f und min: %.2f", front_dist, min_dist);
+        //RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 500,
+        //                     "Vorne: %.2f und min: %.2f", front_dist, min_dist);
     }
 
     double get_min_distance(const sensor_msgs::msg::LaserScan::SharedPtr& msg, 
@@ -64,10 +65,15 @@ private:
             int idx = (i + total) % total; // Wrap around
             if (msg->ranges[idx] > msg->range_min && msg->ranges[idx] < msg->range_max) {
                 min_dist = std::min(min_dist, (double)msg->ranges[idx]);
+                min_idx = idx;
             }
         }
         
         return min_dist;
+    }
+
+    double angle_diff(double a, double b) {
+        return std::atan2(std::sin(a - b), std::cos(a - b));
     }
 
     void control_loop()
@@ -78,25 +84,137 @@ private:
         // Abstand zur Stange vor dem Slalom
         const double PILLER_DIST = 0.3;
 
-        // Zustandsmaschine erst wenn erste Messung da war
-        if(min_dist != 999.0){
-            if(front_dist > min_dist){
-                cmd.twist.linear.x = 0.0;
-                cmd.twist.angular.z = 0.1;
-            }
-            else if (front_dist > PILLER_DIST) {
-                // FALL 1: Stange zu weit weg
-                RCLCPP_INFO(this->get_logger(), "Stange zu weit weg (%.2f)", front_dist);
-                cmd.twist.linear.x = 0.2;
-                cmd.twist.angular.z = 0.0; // Negative = rechts drehen
-            }
-            else {
-                // FALL 2: Vor Stange stehen bleiben            
-                cmd.twist.linear.x = 0.0;
-                cmd.twist.angular.z = 0.0;
+        //Halbkreis
+        const double radius = 0.4;
+        const double v = 0.15; //Geschwindigkeit
+        const double w = v / radius; //Winkelgesch
+
+        switch(state){
+            case 0: //Zur Säule fahren
+            {
+                //wait for bootup
+                if (min_dist == 999.9){
                 
-                RCLCPP_INFO(this->get_logger(), "%.2f vor der Stange zum Stehen gekommen.", 
-                        PILLER_DIST);
+                }
+                else{
+                    if(front_dist > min_dist){
+                        cmd.twist.linear.x = 0.0;
+                        cmd.twist.angular.z = -dir * 0.2;
+                        RCLCPP_INFO(this->get_logger(), "Front Distanz: %.2f, Min Distanz: %.2f", front_dist, min_dist);
+                    }
+                    else if(front_dist > PILLER_DIST){
+                        cmd.twist.linear.x = 0.2;
+                        cmd.twist.angular.z = 0.0;
+                    }
+                    else{
+                        cmd.twist.linear.x = 0.0;
+                        cmd.twist.angular.z = 0.0;
+                        state = 2;
+                        RCLCPP_INFO(this->get_logger(), "State 0 abgeschlossen");
+                    }
+                }
+                break;
+                }
+            case 1: //nach einer Säule zur nächsten fahren
+            {
+                if(front_dist > front_min_dist){
+                    cmd.twist.linear.x = 0.0;
+                    cmd.twist.angular.z = -dir * 0.2;
+                    RCLCPP_INFO(this->get_logger(), "Front Distanz: %.2f, Front Min Distanz: %.2f", front_dist, front_min_dist);              
+                }
+                else if(front_dist > PILLER_DIST){
+                    cmd.twist.linear.x = 0.2;
+                    cmd.twist.angular.z = 0.0;                    
+                }
+                else{
+                    cmd.twist.linear.x = 0.0;
+                    cmd.twist.angular.z = 0.0;
+                    state = 2;
+                    RCLCPP_INFO(this->get_logger(), "State 1 abgeschlossen");
+                    slalom_i += 1;
+                }
+                break;
+            }
+            case 2: //Um 90° drehen
+            {
+                if (!init) {
+                start_yaw = current_yaw;
+                init = true;
+                RCLCPP_INFO(this->get_logger(), "Init gemacht");
+                }
+
+                double delta_yaw = current_yaw - start_yaw;
+
+                if(std::abs(delta_yaw) < M_PI_2){
+                    cmd.twist.linear.x = 0.0;
+                    cmd.twist.angular.z = dir * 0.2;
+                }
+                else{
+                    cmd.twist.linear.x = 0.0;
+                    cmd.twist.angular.z = 0.0;
+                    init = false;
+                    state = 3;
+                    RCLCPP_INFO(this->get_logger(), "State 2 abgeschlossen");
+                    dir = dir * -1;
+                }
+                break;
+            }
+            case 3: //Halbkreis fahren
+            {
+                if (!init) {
+                start_yaw = current_yaw;
+                init = true;
+                RCLCPP_INFO(this->get_logger(), "Init gemacht");
+                }
+                
+                double dyaw = angle_diff(current_yaw, start_yaw);
+                
+                if (slalom_i == 3 && std::abs(dyaw) >= M_PI/2){
+                    cmd.twist.linear.x  = 0.0;
+                    cmd.twist.angular.z = 0.0;
+                    state = 5;
+                    RCLCPP_INFO(this->get_logger(), "State 3 abgeschlossen");
+                    break;
+                }
+
+                if(std::abs(dyaw) < M_PI-0.05){
+                    cmd.twist.linear.x  = v;
+                    cmd.twist.angular.z = dir * w;
+                }
+                else{
+                    cmd.twist.linear.x  = 0.0;
+                    cmd.twist.angular.z = 0.0;
+                    RCLCPP_INFO(this->get_logger(), "State 3 abgeschlossen");
+                    state = 4;
+                    init = false;
+                }
+                break;
+            }
+            case 4: //Anhalten und 45° zurück drehen
+            {
+                if (!init) {
+                    start_yaw = current_yaw;
+                    init = true;
+                    RCLCPP_INFO(this->get_logger(), "Init gemacht");
+                }
+
+                double delta_yaw = current_yaw - start_yaw;
+
+                if(std::abs(delta_yaw) < M_PI_2/2){
+                    cmd.twist.linear.x  = 0.0;
+                    cmd.twist.angular.z = -dir * 0.2;
+                }else{
+                    cmd.twist.linear.x  = 0.0;
+                    cmd.twist.angular.z = 0.0;
+                    RCLCPP_INFO(this->get_logger(), "State 4 abgeschlossen");
+                    state = 1;
+                    init = false;
+                }
+                break;
+            }
+            case 5:
+            {
+                RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 5000, "Ende");
             }
         }
 
@@ -108,10 +226,19 @@ private:
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
     rclcpp::TimerBase::SharedPtr timer_;
 
-    double current_yaw_ = 0.0;
+    double current_yaw = 0.0;
     double front_dist = 999.0;
     double min_dist = 999.0;
-};
+    double front_min_dist = 999.0;
+    int min_idx = -1;
+    //Halbkreis
+    int state = 0;
+    double radius;
+    bool init = false;
+    double start_yaw = 0.0;
+    int dir = -1; // Negative = rechts drehen
+    int slalom_i = 0;
+    };
 
 int main(int argc, char **argv)
 {
